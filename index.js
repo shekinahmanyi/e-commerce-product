7eebdaf0274f714b164bcd024e472e3ec9a12388
shekinahmanyi <shekinahmanyi@gmail.com>
2024-01-25 16:36:02 +0100

    Writing the script for the filter images

---
+++ index.js
@@ -0,0 +1,11 @@
+// // index.js
+// document.addEventListener('DOMContentLoaded', function () {
+//     const filterImages = document.querySelectorAll('.filter-image');
+
+//     filterImages.forEach(image => {
+//         image.addEventListener('click', function () {
+//             const selectedFilterSrc = this.src;
+//             document.querySelector('.section_1 img').src = selectedFilterSrc;
+//         });
+//     });
+// });
